8dbd2d63569e7f1c9365eeafc26f0cf2210370f2
Mohsen Dehbag <33943295+MohsenDehbag@users.noreply.github.com>
2021-02-18 23:26:39 +0330

    Added Ab-Paz 1.0 🍳🥚

---
+++ Beginner/Ab-Paz/EggTimer/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Mohsen 2021-02-18
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["عسلی": 300, "نیم پز": 400, "تمام پز": 720]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = "انتخاب شما هست \(hardness)، خیلی هم عالی"
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        } else {
+            timer.invalidate()
+            titleLabel.text = "تموم!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+            
+        }
+    }
+    
+}
